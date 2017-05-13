33bcec92f8c12acb28193c3b908117dd887b2a32
J-Alexandr <lprankerl@gmail.com>
2017-05-13 20:07:09 +0300

    Implement header based security on custom Filter

---
+++ src/main/java/com/ekreative/hackathon/challenge/filter/HeaderBasedSecurityFilter.java
@@ -0,0 +1,32 @@
+package com.ekreative.hackathon.challenge.filter;
+
+import com.ekreative.hackathon.challenge.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class HeaderBasedSecurityFilter extends GenericFilterBean {
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        try {
+            String uuid = httpRequest.getHeader("UUID");
+            userService.findByUuid(uuid);
+            chain.doFilter(request, response);
+        } catch (Exception e) {
+            ((HttpServletResponse) response).sendError(HttpServletResponse.SC_UNAUTHORIZED, "The token is not valid.");
+        }
+    }
+}
